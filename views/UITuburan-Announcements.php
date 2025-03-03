ce710b81f1d4d109e156c43effa682c5ac0402f2
Laurence Samson <laurencesamsonii@gmail.com>
2025-03-03 12:24:53 +0800

    updated all of the tuburan including the news and events

--- views/UITuburan-Announcements.php
+++ views/UITuburan-Announcements.php
@@ -76,7 +76,31 @@
                       Hills, Cebu City sugod karong lunes pebrero 24 hangtud sa huwebes Pebrero 27. Magapadayon gihapon ang mga skedyol sa weekday masses dinhi sa atong parokya: 6:00 AM, 7:05 AM, 12:00 NN ug 6:00 PM. </p>
 
                 <p>4.	Magasugod na ang panahon sa Kwaresma karong umaabot nga miyerkoles sa badlis (Ash Wednesday) Marso 5. Ang skedyol sa misa mao kining mga mosunod: 6:00 AM, 7:05 Am, 12:NN, 5:00 PM, 6:15 Pm ug 7:30 PM.</p>
-                
+
+              <h3><b> NEWS AND EVENTS </b></h3>
+
+                <p><b> Sacrament of Confirmation </b></p>
+
+                <p> We are pleased to announce that the <b>SACRAMENT OF CONFIRMATION</b> will be administered on January 26, 2025 at 12:00 noon at Diocesan Shrine and Parish of Our Lady of Lourdes. </p>
+                <p>  To all who have not yet received the sacrament are encouraged to register and submit their Baptismal Certificate in the <b>DSPOLL office</b>.</p>
+                <p> This sacred sacrament marks a significant moment in the life of the young people as they receive the Holy Spirit in a special way, as the beginning of a journey of commitment of Christ which strengthens them to live a life of faith, serves and awareness of Christian life. </p>
+                <p> We invite all families, friends and parishioners to join us in commemorating this occasion. </p>
+                <p> For more information, please contact 427-4182 or email us at ourladyoflourdes710@gmail.com or visit the website page of DSPOLL (www.lourdesparishshrine.com).</p>
+
+                <p><b> Feast of Sr. Santo Niño </b></p>
+
+                <p> January 14, 2025 </p>
+                <p> The Santo Niño De Cebu is a Roman Catholic title given to a statue of the infant Jesus that is highly venerated in the Philippines. Considered the oldest Christian relic in the Philippines, the statue was given as a baptismal gift by Portuguese explorer Ferdinand Magellan in year 1521. Decades later, the statue was rediscovered in 1565 after a village fire. The Minore Basillica of Santo Nino was eventually built on the site where it was found. The feast of Santo Niño is celebrated every third Sunday of January with religious processions, sinulog dances and masses over nine 9 days. </p>
+                <p> We heartedly encouraged everyone to join us in celebrating the first-ever SINULOG here in our Parish (Diocesan Shrine and Parish of Our Lady of Lourdes) this coming January 19, 2025, Sunday at 11:30 o’clock in the morning. This Sinulog dance is our way of honoring and expressing gratitude to Santo Niño. In addition, it’s our way of prayer to Santo Niño that he may shower us with blessings, guidance and safety throughout the year 2025. </p>
+
+                <p><b> Solemnity of Mary, Mother of God </b></p>
+
+                <p> December 27, 2024 </p>
+                <p> The Solemnity of Mary, Mother of God, occurs exactly one-week after Christmas, marking the end of the Christmas octave. It is one of the highest ranking feasts in the Church’s liturgical calendar. Celebrated on January 1st, it coincides with the first day of the new year. It is a holy day of obligation for Catholics which means mass attendance is required. It offers an opportunity for the Catholics to begin the year in prayer and reflection, expressing devotion and veneration to Mary, recognizing her as the Mother of Jesus Christ, who is both fully human and divine. </p>
+                <p> The title “Mother of God” (Theotokos means ” God-bearer” in Greek, it acknowledges the profound truth that Mary is the Mother of Jesus Christ. It was formally declared at the council of Ephesus in 431 AD, a key moment in the history of Christian Doctrine. The Solemnity is a reminder of the faithful’s ongoing reflection on the nature of Christ and the deep mystery of the Incarnation. </p>
+                <p> The Solemnity of Mary, Mother of God is a significant event that honors Mary’s crucial role in salvation and Jesus Christ as our Lord and savior. It is a day to contemplate God’s love for humanity, revealed through the birth of Christ, and to honor Mary for her role in that divine plan. </p>
+
+
 
             </div>
           </div>
